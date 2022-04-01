d6e439000e8621598eb42a8ccc9589e166b2acb7
Snouffles <donovan.payan@gmail.com>
2022-04-01 10:56:49 +0200

    some javascript

--- scripts.js
+++ scripts.js
@@ -1,1 +1,17 @@
+let numberOfProduct = document.querySelector(".button__addToCart--number");
+let btnMinus = document.querySelector(".button__addToCart--minus");
+let btnPlus = document.querySelector(".button__addToCart--plus");
+let numberToChange;
 
+console.log(numberToChange);
+
+btnMinus.addEventListener("click", e=>{
+    numberToChange = numberOfProduct.value;
+    if(numberToChange > 1){
+    numberOfProduct.value = numberToChange -1;
+    }
+})
+btnPlus.addEventListener("click", e=>{
+    numberToChange = numberOfProduct.value;
+    numberOfProduct.value = parseInt(numberToChange) +1;
+})
